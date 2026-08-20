b4c2077ce452e6b2dbbd9c198f725af5d5635d5a
adrianaespinar <adrianaespinar.p@gmail.com>
2026-08-20 20:03:55 +0200

    int(input(Preguntar por números matriz 2x2)

--- main.py
+++ main.py
@@ -2,14 +2,12 @@ from modules.matrices import calcular_determinante
 
 def main():
     print("=== 🧮 Math & Utility CLI Suite ===")
-    print("Calculando el determinante de la matriz de prueba...")
     
-    # Llamamos a la función que guardamos en el otro archivo
+    # Llamamos a nuestra función de matrices
     resultado = calcular_determinante()
     
-    # Mostramos el resultado en la pantalla
+    # Mostramos el resultado final
     print(f"El determinante es: {resultado}")
 
-# Esta línea le dice a Python: "ejecuta la función main cuando arranque el archivo"
 if __name__ == "__main__":
     main()
